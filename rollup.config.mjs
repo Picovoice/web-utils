d0ef7a672b512f471c1e95e5a154a003cba9180a
Matthew Maxwell <matthew@picovoice.ai>
2026-06-29 13:24:12 -0700

    fix

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,6 +8,10 @@ import pkg from './package.json' with { type: 'json' }
 import { babel } from '@rollup/plugin-babel'
 import terser from '@rollup/plugin-terser'
 import { DEFAULT_EXTENSIONS } from '@babel/core'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const extensions = [...DEFAULT_EXTENSIONS, '.ts'];
 
@@ -26,27 +30,27 @@ const iifeBundleName = pkg.name
 console.log(iifeBundleName);
 
 export default {
-  input: [path.resolve(import.meta.dirname, pkg.entry)],
+  input: [path.resolve(__dirname, pkg.entry)],
   output: [
     {
-      file: path.resolve(import.meta.dirname, pkg['module']),
+      file: path.resolve(__dirname, pkg['module']),
       format: 'esm',
       sourcemap: false,
     },
     {
-      file: path.resolve(import.meta.dirname, 'dist', 'esm', 'index.min.js'),
+      file: path.resolve(__dirname, 'dist', 'esm', 'index.min.js'),
       format: 'esm',
       sourcemap: false,
       plugins: [terser()],
     },
     {
-      file: path.resolve(import.meta.dirname, pkg.iife),
+      file: path.resolve(__dirname, pkg.iife),
       format: 'iife',
       name: iifeBundleName,
       sourcemap: false,
     },
     {
-      file: path.resolve(import.meta.dirname, 'dist', 'iife', 'index.min.js'),
+      file: path.resolve(__dirname, 'dist', 'iife', 'index.min.js'),
       format: 'iife',
       name: iifeBundleName,
       sourcemap: false,
@@ -58,7 +62,7 @@ export default {
     commonjs(),
     workerLoader({ targetPlatform: 'browser', sourcemap: false }),
     typescript({
-      cacheRoot: path.resolve(import.meta.dirname, '.rts2_cache'),
+      cacheRoot: path.resolve(__dirname, '.rts2_cache'),
       clean: true,
     }),
     babel({
